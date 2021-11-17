8676bbf6d8b3c662e4fe5285addba16430fd001d
Daniel Xutuc <danielxutuc@gmail.com>
2021-11-17 02:52:18 -0300

    moved the old patients page to an index

---
+++ pages/patients/index.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <template>           
+            <v-container>
+                <v-card>
+                    <template>
+                        <v-toolbar flat>
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary" class="mb-2" @click="$router.push('/patients/new')">
+                                Nuevo Paciente
+                            </v-btn>            
+                        </v-toolbar>
+                    </template>
+                    <v-card-title>
+                        <v-text-field v-model="search" append-icon="mdi-magnify" label="Search" single-line hide-details></v-text-field>
+                    </v-card-title>               
+                    <v-data-table :headers="headers" :items="patients" @click:row="verPaciente" :search="search"></v-data-table>
+                </v-card>                       
+            </v-container>
+        </template>
+    </div>    
+</template>
+
+<script>
+
+    export default {
+        data () {
+        return {
+            search: '',
+            headers: [
+                {text: 'ID', value: 'id', sortable: false},
+                {text: 'Nombre', value: 'first_name'},
+                {text: 'Apellido', value: 'last_name'},
+                {text: 'Ordenes', value: 'orders'} 
+            ],
+
+            patients: [],
+
+            patient: {},
+        }
+        },
+
+        mounted() {
+            this.getAllPacientens()
+        },
+
+        methods: {
+            async getAllPacientens() {
+                const {data} = await this.$axios.get(`patients`)
+                this.patients = data
+            },
+            verPaciente(dato, item){
+                this.$router.push('/patients/'+dato.id);
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
