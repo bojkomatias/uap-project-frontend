29453e1094c386373877472c250139aac46fee3e
Boris <borissicsac@gmail.com>
2021-12-08 17:46:07 -0300

    Custom login kawaii

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,49 @@
 <template>
-  <div class="d-flex align-center justify-center dentalImg">
+  <div id="bgDental" class="d-flex align-center justify-center dentalImg">
     <div class="d-flex justify-center align-center flex-column">
       <div class="py-16 blured d-inline-flex align-center justify-center">
         <h1 class="primary--text display-1 text-h1 text-center">
           Administrador de Prácticas Odontológicas
         </h1>
       </div>
-      <v-btn x-large color="primary" class="mt-6">
-        <div class="d-flex justify-center mr-4 microsoft"></div>
-        Iniciar sesión con Microsoft
-      </v-btn>
+      <div class="mt-6 pa-8 bluredShort flex-column rounded-xl">
+        <v-layout >
+          <v-flex >
+            <v-card class="elevation-12">
+              <v-toolbar class="d-flex justify-center align-center" dark color="primary" >
+                <v-toolbar-title>Inicio de Sesión</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text >
+                <v-form>
+                  <v-text-field
+                    prepend-icon="mdi-account"
+                    name="login"
+                    label="Usuario"
+                    type="text"
+                  ></v-text-field>
+                  <v-text-field
+                    id="password"
+                    prepend-icon="mdi-lock"
+                    name="password"
+                    label="Contraseña"
+                    type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions  class="flex-column">
+                <v-spacer></v-spacer>
+                <v-btn class="px-10"  color="primary">Iniciar Sesión</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+        <h3 class=" d-flex justify-center align-center pt-6"> <v-divider class="mx-2" color="black"></v-divider> O <v-divider class="mx-2" color="black"></v-divider></h3>
+        <v-btn x-large color="primary" class="mt-6">
+          
+          <div class="d-flex justify-center mr-4 microsoft"></div>
+          Iniciar sesión con Microsoft
+        </v-btn>
+      </div>
     </div>
   </div>
 </template>
@@ -21,22 +55,31 @@ export default {
 </script>
 
 <style scoped>
+span { white-space: nowrap }
 .blured {
-  width: 190vh;
+  width: 120%;
+  background-color: rgba(255, 255, 255, 0.1);
+  backdrop-filter: blur(8px) saturate(70%) contrast(45%) brightness(110%);
+}
+
+.bluredShort {
   background-color: rgba(255, 255, 255, 0.1);
   backdrop-filter: blur(8px) saturate(70%) contrast(45%) brightness(110%);
 }
 .dentalImg {
   height: 100vh;
-  background-position: center; /* Center the image */
-  background-repeat: no-repeat; /* Do not repeat the image */
-  background-size: cover; /* Resize the background image to cover the entire container */
-  background-image: url('https://images.unsplash.com/photo-1598256989800-fe5f95da9787?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url(static\dentalImg.jpg);
 }
+
 .microsoft {
   height: 2.5vh;
   width: 2.5vh;
   background-size: cover;
   background-image: url(static\microsoftLogo.svg);
 }
+
+
 </style>
